33e51a60fab9b156427f8bfa8c31f873b6852e8f
Lee Bardon <leerbardon@gmail.com>
2019-12-28 20:17:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  array.each do |i|
+    new_array << yield(i)
+  end
+  new_array
+end
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    num = starting_point
+    i = 0
+  else 
+    num = array[0]
+    i = 1
+  end
+  while i < array.length
+    num = yield(num, i)
+  end
+  i += 1 
+  num
+end
+
+
