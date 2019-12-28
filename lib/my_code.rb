e9e4876fa3cb039c20834ee2ad21477b46da7328
Lee Bardon <leerbardon@gmail.com>
2019-12-28 20:33:01 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  array.each do |i|
+    new_array << yield(i)
+  end
+  new_array
+end
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    n = starting_point
+    i = 0
+  else 
+    n = array[0]
+    i = 1
+  end
+  while i < array.length
+    n = yield(n, array[i])
+  i += 1 
+  end
+  n
+end
+
+=begin
+def reduce(array, starting_point = nil)
+  result = 0
+  array.each do |i|
+    result = yield(result, i)
+  end
+  if starting_point
+    result += starting_point
+  end
+  result
+end
+=end   # problems with passing the truthy/falsy q's
